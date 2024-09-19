7259b1c7a8f20d136b689edb49ab767ccf79936e
BhushanBagde123 <bhushnabagade7@gmail.com>
2024-09-19 21:22:03 +0530

    modified: update routes

--- src/component/ArrowButton.jsx
+++ src/component/ArrowButton.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
 
-function ArrowButton() {
+function ArrowButton({onClick,direction}) {
   return (
-    <div>ArrowButton</div>
+    <div 
+    className={`absolute ${direction === 'next' ? 'right-5' : 'left-5'} top-1/2 transform -translate-y-1/2 z-10 cursor-pointer`}
+    onClick={onClick}
+  >
+    {direction === 'next' ? 
+      <FaArrowRight size={30} className="text-white bg-black rounded-full p-2" /> :
+      <FaArrowLeft size={30} className="text-white bg-black rounded-full p-2" />
+    }
+  </div>
+  
   )
 }
 
